817fd7b70ce546d301ebf58053fe888568691c9b
fastapi-backend <formok09@gmail.com>
2024-08-02 23:30:09 +0300

    Init alembic

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
-from model import core
-from model.database import engine
 from routers.items import router as items_router
 from routers.users import router as users_router
 
-core.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
