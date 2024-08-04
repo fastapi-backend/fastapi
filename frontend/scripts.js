a690bb9b0e0c1593d7704d128136623cb4cb3bbc
fastapi-backend <formok09@gmail.com>
2024-08-04 20:13:29 +0300

    commit

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -14,15 +14,26 @@ const passwordInput = document.querySelector('#password')
 const loginBtn = document.querySelector('#login')
 const loginWrp = document.querySelector('#login-wrp')
 const userSection = document.querySelector('#user-section')
+const invalidSection = document.querySelector('#invalid_username')
+const InfoSection = document.querySelector('#info_user_div')
+const dataContainer = document.querySelector('#info');
 const welcome = document.querySelector('#welcome')
+const invalid = document.querySelector('#invalid')
 const logoutBtn = document.querySelector('#logout')
 
 const currentUser = () => {
     if (localStorage.getItem('username')) {
         loginWrp.style.display = 'none'
         userSection.style.display = 'block'
+        invalidSection.style.display = 'none'
+        invalid.innerHTML = ``
         welcome.innerHTML = `Hi, <strong>${localStorage.getItem('username')}</strong>!`
     }
+}
+const error_notfound = () => {
+        invalidSection.style.display = 'block'
+        invalid.innerHTML = `Invalid username or password!!`
+
 }
 
 currentUser()
@@ -31,6 +42,8 @@ logoutBtn.addEventListener('click', () => {
     localStorage.removeItem('token')
     localStorage.removeItem('username')
 
+    InfoSection.style.display = 'block';
+    dataContainer.innerHTML = ''
     loginWrp.style.display = 'block'
     userSection.style.display = 'none'
     welcome.innerHTML = ''
@@ -42,25 +55,41 @@ loginBtn.addEventListener('click', () => {
     formData.append('username', usernameInput.value)
     formData.append('password', passwordInput.value)
 
-    fetch('http://127.0.0.1:8000/test/token/', {
-        method: 'POST',
-        body: formData
+fetch('http://127.0.0.1:8000/test/token/', {
+    method: 'POST',
+    body: formData
+})
+    .then(response => {
+        if (!response.ok) {
+            if (response.status === 404){
+                error_notfound()
+                console.log('Invalid username or password')
+            }
+            if (response.status === 422){
+                error_notfound()
+                console.log('Invalid username or password')
+            }
+
+            throw new Error('Network response was not ok.');
+        }
+        return response.json();
     })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+    .then(data => {
+        console.log(data)
 
-            localStorage.setItem('token', data.access_token)
-            localStorage.setItem('username', data.username)
+        localStorage.setItem('token', data.access_token)
+        localStorage.setItem('username', data.username)
 
-            currentUser()
-        })
-        .catch(error => console.error(error))
+        currentUser()
+    })
+    .catch(error => console.error(error))
 })
 
 
 
+
 const Profile_token = document.querySelector('#get-profile-token')
+
 
 let token = localStorage.getItem('token')
 
@@ -72,6 +101,23 @@ Profile_token.addEventListener('click', () => {
         },
     })
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            InfoSection.style.display = 'block';
+            dataContainer.innerHTML = ''
+            for (const key in data) {
+                const paragraph = document.createElement('p');
+                paragraph.textContent = `${key}: ${data[key]}`;
+                dataContainer.appendChild(paragraph);
+            }
+            InfoSection.appendChild(dataContainer);
+            console.log(data);
+        })
+
+
+
+
+
         .catch(error => console.error(error))
-})
+        })
+
+
